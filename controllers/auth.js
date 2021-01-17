bff8a36d8168302aed99375cc414ae0442be1087
UsmanSarwar <usmansarwar672@gmail.com>
2021-01-17 04:28:34 -0800

    gtuser

--- controllers/auth.js
+++ controllers/auth.js
@@ -76,7 +76,7 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
 const hostel = await Hostel.findOne({user: req.user.id});
  
-  res.status(200).json({ success: true, data: {user,hostel});
+  res.status(200).json({ success: true, data: {user,hostel}});
 });
 
 // @desc        Update user details
